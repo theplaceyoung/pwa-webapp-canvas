79865503f3e49a5138bf2c7679b76ceeef4d876d
Jang So Young <theplaceyoung@gmail.com>
2025-01-28 13:27:20 +0900

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './script.js';
-import './drag-and-drop.js';
-import './toggleScrollLock.js';
-import './scrollLockUI.js';
-import '../service-worker.js';
-
-console.log("Hello from index.js!");
